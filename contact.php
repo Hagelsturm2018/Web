94844156614a6688dfadcd698f7411eb5021e6d4
Jannik Schilling <hagelstumm1234@gmail.com>
2020-05-04 21:55:15 +0200

    PHP

--- contact.php
+++ contact.php
@@ -1,10 +1,10 @@
 <?php
- $email= htmlspecialchars($_POST[cEmail]);
- $message = htmlspecialchars($_POST[cMessage]);
- $sender= htmlspecialchars($_POST[cName]);
- if(!empty($email) && !empty($message)) {
-   if (@mail("hagelstumm1234@gmail.com", "Message: ", $message, "E-Mail: $email", "Sender: $sender")) { echo "Die Nachricht wurde abgeschickt";
- } else { 
+$email= htmlspecialchars($_POST[cEmail]);
+$message = htmlspecialchars($_POST[cMessage]);
+$sender= htmlspecialchars($_POST[cName]);
+if(!empty($email) && !empty($message)) {
+    if (@mail("hagelstumm1234@gmail.com", "Message: ", $message, "E-Mail: $email", "Sender: $sender")) { echo "Die Nachricht wurde abgeschickt";
+} else { 
    echo "Es gab einen Fehler";
  }
  }
